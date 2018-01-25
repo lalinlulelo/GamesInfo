6d8fe93cb9e29dd9e50d41bdbdee6ffbddc55c91
guille <guillemelmor@gmail.com>
2018-01-25 17:28:55 +0100

    limpieza de codigo
    he limpiado el codigo, mirad a ver si lo recibis

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/InitController.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/InitController.java
@@ -3,10 +3,10 @@ package LosOdiosos3.prueba_servidor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-//tag de la pagina: localhost:8080/init
+
 @Controller
 public class InitController {
-	
+	//tag de la pagina: localhost:8080/init
 	@RequestMapping("/init")
 	public String init (Model model) {
 		model.addAttribute("name","World");
