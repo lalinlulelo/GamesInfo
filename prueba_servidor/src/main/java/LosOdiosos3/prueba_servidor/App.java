148402e5443cb3c751359d9fe0b68a813d9bcf8d
lalinlulelo <lalinlulelo@gmail.com>
2018-01-25 19:14:19 +0100

    prueba
    prueba

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/App.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        // comentario de prueba
+        // comentario de prueba 222
     	SpringApplication.run(App.class, args);
     }
 }
