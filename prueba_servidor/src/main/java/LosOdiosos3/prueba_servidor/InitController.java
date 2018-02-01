f83582329be06ffe38b6310ada599100a3849c61
guille <guillemelmor@gmail.com>
2018-02-01 21:52:19 +0100

    Inicio de Sesion (no se si correcto del todo) funcionando

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/InitController.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/InitController.java
@@ -2,45 +2,46 @@ package LosOdiosos3.prueba_servidor;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InitController {
 	
-	// ----------------------------- INYECCIONES -------------------------------------
-	// identifica la sesion de un usuario
-	@Autowired
-	private User usuario;
-	
+	// ----------------------------- INYECCIONES -------------------------------------	
 	// repositorio de la tabla usuarios
 	@Autowired
-	private UserRepository repository;
+	private UserRepository repository;	
 	// ------------------------------ FIN INYECCIONES --------------------------------
 	
 	// ----------------------------- PAGINA INICIO -----------------------------------
 	@RequestMapping("/")
-	public String inicio (Model model) {
+	public String inicio (Model model, HttpSession usuario) {
+		repository.save(new User("Juan", "123", "rojo"));
+		repository.save(new User("francisco", "aupa", "amarillo"));
+		// comentarios de prueba de la pagina html
 		model.addAttribute("Titulo", "Agus guapo");
 		model.addAttribute("Cuerpo", "Susa idiota");
+		
 		// se muestra el link de iniciar/registrar usuario si es false
-		model.addAttribute("registered", usuario.getRegistered());
-		//model.addAttribute("sin_registrar", sin_registrar);
-		//model.addAttribute("registrado", registrado);
+		model.addAttribute("registered", usuario.getAttribute("registered"));
+		
 		return "index";
 	}
 	// ----------------------------- FIN PAGINA INICIO -------------------------------
 	
 	// ----------------------------- REGISTRAR NUEVO USUARIO -------------------------
-	@PostMapping(value = "/registrar")
-	public String registrar(Model model, User_Aux user) {
-		usuario.setName(user.getName());
-		usuario.setPassword(user.getPassword());
-		usuario.setDate(user.getDate());
+	@PostMapping(value = "/register")
+	public String registrar(Model model, User user, HttpSession usuario) {
+		// se inicializa el usuario con los datos del formulario
+		usuario.setAttribute("name", user.getName());
+		usuario.setAttribute("password", user.getPassword());
+		usuario.setAttribute("date", user.getDate());
 		
 		// se comprueba la existencia del nombre
 		List<User> usur = repository.findByName(user.getName());
@@ -51,19 +52,21 @@ public class InitController {
 		}
 		
 		// se da por registrado al usuario
-		usuario.setRegistered(true);
-		repository.save(usuario);
+		usuario.setAttribute("registered", true);
+		repository.save(new User(user.getName(), user.getPassword(), user.getDate()));
 		
 		// se muestra MOSTRAR PERFIL si es true
-		model.addAttribute("registered", usuario.getRegistered());
+		model.addAttribute("registered", usuario.getAttribute("registered"));
+		model.addAttribute("Titulo", "Agus guapo");
+		model.addAttribute("Cuerpo", "Susa idiota");
 		return "index";
 	}
 	// ----------------------------- FIN REGISTRAR NUEVO USUARIO ---------------------
 	
 	// ----------------------------- INICIO DE SESION --------------------------------
-	@RequestMapping("/start_session")
-	public String iniciar_sesion (Model model, User_Aux user) {
-		
+	@RequestMapping("/login")
+	public String iniciar_sesion (Model model, User user, HttpSession usuario) {
+		System.out.println(user.getName());
 		// recopilamos la lista de usuarios que contienen el nombre
 		// --- en teoria no puede haber varios con el mismo nombre ---
 		List<User> usur = repository.findByName(user.getName());
@@ -74,17 +77,21 @@ public class InitController {
 		// con las disponibles
 		if(usur.size() > 0) {
 			for(int i = 0; i < usur.size(); i++) {
+				System.out.println(usur.get(i).toString());
 				if(user.getPassword().equals(usur.get(i).getPassword())) {
 					aux = usur.get(i);
+					usuario.setAttribute("registered", true);
 					break;
 				}
 			}
+			usuario.setAttribute("registered", false);
 		}else {
 			// en caso de no existir el usuario, se ha de notificar de algun modo
 			// --- procesar cual seria el correcto ---
 			System.out.println("user or password wrong");
+			usuario.setAttribute("registered", false);
 		}
-		
+		model.addAttribute("loged", usuario.getAttribute("registered"));
 		// se dirige a la pagina como iniciado
 		return "loged";
 	}
@@ -92,12 +99,12 @@ public class InitController {
 	
 	// ----------------------------- PERFIL DE USUARIO -------------------------------
 	@RequestMapping("/profile")
-	public String init (Model model) {
+	public String init (Model model, HttpSession usuario) {
 		
 		// se cogen del usuario los atributos
-		String name = usuario.getName();
-		String password = usuario.getPassword();
-		String date = usuario.getDate();
+		String name = (String) usuario.getAttribute("name");
+		String password = (String) usuario.getAttribute("password");
+		String date = (String) usuario.getAttribute("date");
 		
 		// se mandan los datos al modelo
 		model.addAttribute("name", name);
--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/User.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/User.java
@@ -9,27 +9,33 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
 // se indica que es un componente
-
-@Component
-@SessionScope
 @Entity
 public class User {
-	// id de la entidad user
 	
+	// id de la entidad user	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
 	// atributos del usuario
-	private String name;
-	private String password;
-	private String date;
-	private boolean registered = false;
+	private String name = "...";
+	private String password = "...";
+	private String date = "...";
 	
 	// lista de juegos
 	// suscripcion juego
 	// suscripcion compañia
 	// suscripcion evento
+	
+	// constructor
+	protected User () {}
+	
+	// contructor
+	public User (String name, String password, String date) {
+		this.name = name;
+		this.password = password;
+		this.date = date;
+	}
 	
 	// setters
 	public void setName (String name) {
@@ -42,10 +48,6 @@ public class User {
 	
 	public void setDate (String date) {
 		this.date = date;
-	}
-	
-	public void setRegistered (boolean valor) {
-		registered = valor;
 	}
 	
 	// getters
@@ -61,7 +63,7 @@ public class User {
 		return date;
 	}
 	
-	public boolean getRegistered() {
-		return registered;
+	public String toString () {
+		return "name: " + name + "/npassword: " + password + "/nbirthday: " + date;
 	}
 }
