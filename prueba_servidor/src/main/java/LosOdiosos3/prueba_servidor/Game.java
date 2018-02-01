f292f00e19326c1b4302686bbcf2e6d3ce4a207b
lalinlulelo <lalinlulelo@gmail.com>
2018-02-01 12:13:49 +0100

    Prueba con Game

---
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Game.java
@@ -0,0 +1,45 @@
+package LosOdiosos3.prueba_servidor;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+
+@Entity
+public class Game {
+	@Id
+	
+	private long id;
+	
+	private String name;
+	
+	@ManyToOne
+	private Company company;
+	private String companyName;
+	private String genre;
+	private int year;
+	private Date addGame;
+	
+	@OneToOne(cascade=CascadeType.REMOVE)
+	private Image image;
+	
+	private String url;
+	
+	public Game(String name, Company company,String companyName, String genre, int year, Date addGame,
+			Image image, String url) {		
+		
+		this.name = name;
+		this.company = company;
+		this.companyName= companyName;
+		this.genre = genre;
+		this.year = year;
+		this.addGame = addGame;
+		this.imagen = imagen;
+		this.url = url;
+	}
+
+}
--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Juego.java
+++
@@ -1,7 +0,0 @@
-package LosOdiosos3.prueba_servidor;
-import javax.persistence.Entity;
-
-@Entity
-public class Juego {
-
-}
