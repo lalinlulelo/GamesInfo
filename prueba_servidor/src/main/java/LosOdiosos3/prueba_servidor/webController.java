01a380cf879e98fae7c042eccbd5f6a5c370a3f0
guille <guillemelmor@gmail.com>
2018-02-09 21:58:34 +0100

    Busqueda

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/webController.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/webController.java
@@ -568,21 +568,110 @@ public class webController {
 		model.addAttribute("alert"," ");
 		model.addAttribute("hello", " ");
 		model.addAttribute("Titulo", " ");
-
-		model.addAttribute("Cuerpo", " ");
-		
+		model.addAttribute("Cuerpo", " ");			
 		
 		// se retorna al juego
-
 		return "/game/"+game.getName();
 	}
+	// ----------------------------- FIN COMENTARIOS  --------------------------------------------------
+	
+	// -------------------------------- BUSQUEDAS ------------------------------------------------------
+	@RequestMapping("/search")
+	public String search (Model model, HttpSession usuario, @RequestParam String text) {
+		System.out.println(text);
+		List<Game> games = gameRepository.findByNameContaining(text);
+		List<Company> companies = companyRepository.findByNameContaining(text);
+		List<Event> events = eventRepository.findByNameContaining(text);
+		
+		List<String> list_games = new ArrayList<String>();
+		List<String> list_companies = new ArrayList<String>();	
+		List<String> list_events = new ArrayList<String>();	
+		
+		boolean n_games = false;
+		boolean n_companies = false;
+		boolean n_events = false;
+		boolean no_lists = true;
+		System.out.println(games.size());
+		System.out.println(companies.size());
+		System.out.println(events.size());
+		if(games.size() > 0) {
+			no_lists = false;
+			n_games = true;
+			String div="<div class=\"col-md-3\">\r\n" + "<div class=\"Game\">\r\n" + "<img src=\"%s\" class=\"imagen\">\r\n" + 	" <a href=\"%s\" style=\"text-align:center;display:block; color:  rgb(33, 73, 138);\">%s</a>\r\n" + "     \r\n" + "    </div>\r\n" + "  </div>";
+			// lista de todas las compañias disponibles		
+			for(int i=0; i < games.size(); i++) {
+				//Aqui accederiamos a la base de datos para cambiar en cada iteracion las variables
+				String Url = games.get(i).getImage();
+				System.out.println(Url);
+				String Titulo = games.get(i).getName();
+				System.out.println(Titulo);
+				String link = "/game/" + Titulo;			
+				//Copiamos el div que queremos poner en el documento html en una variable auxiliar
+				//Le damos formato a la variable auxiliar y la añadimos a la lista
+				String aux = String.format(div, Url, link, Titulo);
+				list_games.add(aux);			
+			}			
+			// se transmite la lista a la plantilla
+			model.addAttribute("games", list_games);
+		}
+		
+		if(companies.size() > 0) {
+			no_lists = false;
+			n_companies = true;
+			String div="<div class=\"col-md-3\">\r\n" + "<div class=\"Game\">\r\n" + "<img src=\"%s\" class=\"imagen\">\r\n" + 	" <a href=\"%s\" style=\"text-align:center;display:block; color:  rgb(33, 73, 138);\">%s</a>\r\n" + "     \r\n" + "    </div>\r\n" + "  </div>";
+			// lista de todas las compañias disponibles		
+			for(int i=0; i < companies.size(); i++) {
+				//Aqui accederiamos a la base de datos para cambiar en cada iteracion las variables
+				String Url = companies.get(i).getImage();
+				String Titulo = companies.get(i).getName();
+				String link = "/company/" + Titulo;			
+				//Copiamos el div que queremos poner en el documento html en una variable auxiliar
+				//Le damos formato a la variable auxiliar y la añadimos a la lista
+				String aux=String.format(div, Url, link, Titulo);
+				list_companies.add(aux);			
+			}			
+			// se transmite la lista a la plantilla
+			model.addAttribute("companies", list_companies);
+		}
+		
+		if(events.size() > 0) {
+			no_lists = false;
+			n_events = true;
+			String div="<div class=\"col-md-3\">\r\n" + "<div class=\"Game\">\r\n" + "<img src=\"%s\" class=\"imagen\">\r\n" + 	" <a href=\"%s\" style=\"text-align:center;display:block; color:  rgb(33, 73, 138);\">%s</a>\r\n" + "     \r\n" + "    </div>\r\n" + "  </div>";
+			// lista de todas las compañias disponibles		
+			for(int i=0; i < events.size(); i++) {
+				//Aqui accederiamos a la base de datos para cambiar en cada iteracion las variables
+				String Url = events.get(i).getImage();
+				String Titulo = events.get(i).getName();
+				String link = "/event/" + Titulo;			
+				//Copiamos el div que queremos poner en el documento html en una variable auxiliar
+				//Le damos formato a la variable auxiliar y la añadimos a la lista
+				String aux=String.format(div, Url, link, Titulo);
+				list_events.add(aux);			
+			}			
+			// se transmite la lista a la plantilla
+			model.addAttribute("events", list_events);
+		}
+		model.addAttribute("no_lists", no_lists);
+		model.addAttribute("n_games", n_games);
+		model.addAttribute("n_companies", n_companies);
+		model.addAttribute("n_events", n_events);
+		
+		// se pasan los atributos de la barra de navegacion
+		model.addAttribute("registered", usuario.getAttribute("registered"));
+		boolean aux = !(Boolean) usuario.getAttribute("registered");
+		model.addAttribute("unregistered", aux);
+		model.addAttribute("name", usuario.getAttribute("name"));
+		model.addAttribute("alert"," ");
+		model.addAttribute("hello", " ");
+		model.addAttribute("Titulo", " ");
+		model.addAttribute("Cuerpo", " ");	
+		
+		return "search";
+	}
+
+}	
 
 	
-
-	}	
-
-
-
-	// ----------------------------- FIN COMENTARIOS  --------------------------------------------------
 
 
