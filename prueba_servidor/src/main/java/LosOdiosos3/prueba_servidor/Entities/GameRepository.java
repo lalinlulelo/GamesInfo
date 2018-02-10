c2b572c1747fd86aa87c3a210b529a2c582fb22c
guille <guillemelmor@gmail.com>
2018-02-10 18:44:36 +0100

    o

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Entities/GameRepository.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Entities/GameRepository.java
@@ -3,6 +3,7 @@ import LosOdiosos3.prueba_servidor.*;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
