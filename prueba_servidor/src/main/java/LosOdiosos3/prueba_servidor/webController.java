05ba875f880867b504db161670189c211332f898
Agus <agustwin@gmail.com>
2018-02-13 17:19:49 +0100

    Compañia con imagen en los comentarios

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/webController.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/webController.java
@@ -476,7 +476,7 @@ public class webController {
 				
 			// gestion de commentarios del juego
 			List<String> list=new ArrayList<String>();
-			String div="<div class=\"com\"><div class=\"commentsUser \">%s</div> <div class=\" Date\">%s</div>\r\n" +  "     <div class=\"comments \">%s</div>"	+ "</div><br>";
+			String div="<div class=\"com\"><div class=\"commentsUser \"><img class=\"comment_img\" src=\"%s\"></img>%s</div><div class=\"Date\">%s</div></div>\r\n" +  "     <div class=\"comments \">%s</div>"	+ "</div><br>";
 			
 			// si hay comentarios en el juego
 			if(companies.get(0).getComment().size()>0) {
@@ -486,11 +486,11 @@ public class webController {
 					//Aqui accederiamos a la base de datos para cambiar en cada iteracion las variables
 					String User=list_comments.get(i).getUser().getName();
 					String Text=list_comments.get(i).getText();
-					Date d=list_comments.get(i).getDate();
-					
+					Date d=list_comments.get(i).getDate();	
+					String img=list_comments.get(i).getUser().getIcon();
 					//Copiamos el div que queremos poner en el documento html en una variable auxiliar
 					//Le damos formato a la variable auxiliar y la añadimos a la lista
-					String aux=String.format(div, User,d.toString() ,Text);				
+					String aux=String.format(div, img,User,d.toString(), Text);						
 					list.add(aux);				
 				}
 				Collections.reverse(list);
