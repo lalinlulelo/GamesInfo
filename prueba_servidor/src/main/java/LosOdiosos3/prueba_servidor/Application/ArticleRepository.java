302f07842833d3b68ff0bf6496631b5cb714fa8d
Agus <agustwin@gmail.com>
2018-02-22 11:45:38 +0100

    Ya vuelve a funcionar

---
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/ArticleRepository.java
@@ -0,0 +1,13 @@
+package LosOdiosos3.prueba_servidor.Application;
+
+import java.util.List;
+import LosOdiosos3.prueba_servidor.Application.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+	List<Article> findByTitle(String title);
+
+}
+
+
+
---
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/MyList.java
@@ -0,0 +1,58 @@
+package LosOdiosos3.prueba_servidor.Application;
+
+import java.util.ArrayList;
+import java.util.List;
+import LosOdiosos3.prueba_servidor.Application.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class MyList {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	// una lista puede tener varios juegos
+	@ManyToMany(mappedBy="lists")
+	private List<Game> games = new ArrayList<Game>();
+	
+	// un usuario puede tener varias listas
+	@ManyToOne
+	private User user;
+	
+	private String name;
+	
+	public MyList () {}
+	
+	public MyList (String name) {
+		this.name = name;
+	}
+	
+	public void cleanList () {
+		this.games.clear();
+	}
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+	
+	public String getName () {
+		return name;
+	}
+	
+	public void addGame (Game game) {
+		games.add(game);
+	}
+	
+	public List<Game> getList (){
+		return games;
+	}
+	
+	public void addUser(User user) {
+		this.user = user;
+	}
+}
