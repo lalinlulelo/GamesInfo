bc0a55f3977ca536440d1ec111970390fab61aa5
lalinlulelo <lalinlulelo@gmail.com>
2018-03-26 15:49:52 +0200

    Version con cache

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/App.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/App.java
@@ -1,12 +1,56 @@
 package LosOdiosos3.prueba_servidor.Application;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+
+@EnableCaching
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class App {
     public static void main( String[] args ){    	
     	SpringApplication.run(App.class, args);
+    }
+    
+    @Value("${hazelcast.member}")
+	private String HAZELCAST_MEMBER;
+    
+    private static final Log LOG = LogFactory.getLog(App.class);
+
+    @Bean
+    public Config config() {
+    	Config config = new Config();
+    	NetworkConfig network = config.getNetworkConfig();
+    	
+    	JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+    	joinConfig.getMulticastConfig().setEnabled(false);
+    	joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList(HAZELCAST_MEMBER));
+    	network.getInterfaces().setEnabled(true).addInterface(HAZELCAST_MEMBER);
+    	
+    	return config;
+    }
+    
+    @Bean
+    public CacheManager cacheManager() {
+    	LOG.info("Activating cache...");
+    	return new ConcurrentMapCacheManager("games");
     }
 }
---
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/cacheController.java
@@ -0,0 +1,23 @@
+package LosOdiosos3.prueba_servidor.Application;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class cacheController {
+
+	@Autowired
+	private CacheManager cacheManager;
+	
+	@RequestMapping(value="/cache", method=RequestMethod.GET)
+	public Map<Object, Object> getCacheContent() {
+		ConcurrentMapCacheManager cacheMgr = (ConcurrentMapCacheManager) cacheManager;
+		ConcurrentMapCache cache = (ConcurrentMapCache) cacheMgr.getCache("games");
+		return cache.getNativeCache();
+	}
+}
