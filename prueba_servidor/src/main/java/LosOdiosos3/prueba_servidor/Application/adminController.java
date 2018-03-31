de18c15be6e747fb06e85edf189b30ea20a3bc8b
guille <guillemelmor@gmail.com>
2018-03-31 19:11:42 +0200

    cambios realizados durante el segundo balanceador

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/adminController.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/adminController.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+ 
 @Controller
 public class adminController {
 	// ----------------------------- INYECCIONES --------------------------------------	
