d5c2ddedea2998b5a57d10d2ce3624e3db2e82f6
Agus <agustwin@gmail.com>
2018-04-12 21:11:36 +0200

    guarda eventos en cache

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/EventRepository.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/EventRepository.java
@@ -2,12 +2,17 @@ package LosOdiosos3.prueba_servidor.Application;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@CacheConfig(cacheNames="games")
 public interface EventRepository extends JpaRepository<Event, Long> {
 	// busqueda con nombre
+	//@Cacheable
 	List<Event> findByName(String name);
 	
 	// busqueda con nombre parcial
+	//@Cacheable
 	List<Event> findByNameContaining(String name);
 }
