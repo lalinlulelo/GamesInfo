ef75ece15d361fd17c09e74224e9be25f072d7a4
Agus <agustwin@gmail.com>
2018-04-12 22:10:54 +0200

    borra cache al modificar eventos

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/EventRepository.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/EventRepository.java
@@ -3,11 +3,19 @@ package LosOdiosos3.prueba_servidor.Application;
 import java.util.List;
 
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @CacheConfig(cacheNames="games")
 public interface EventRepository extends JpaRepository<Event, Long> {
+	
+	@CacheEvict(allEntries=true)
+	Event save(Event event);
+	
+	@CacheEvict(allEntries=true)
+	void delete(Event event);
+	
 	// busqueda con nombre
 	@Cacheable
 	List<Event> findByName(String name);
