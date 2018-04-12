9bfb2de589b56090302d36e0778e54d5c8826866
Agus <agustwin@gmail.com>
2018-04-12 20:49:05 +0200

    Cachea compañias

--- prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/CompanyRepository.java
+++ prueba_servidor/src/main/java/LosOdiosos3/prueba_servidor/Application/CompanyRepository.java
@@ -2,15 +2,25 @@ package LosOdiosos3.prueba_servidor.Application;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+@CacheConfig(cacheNames="games")
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 	
+	@CacheEvict(allEntries=true)
+	Company save(Company company);
+	
+	
 	// busqueda por nombre
+	@Cacheable
 	List<Company> findByName(String name);
 	
 	// busqueda con nombre parcial
+	@Cacheable
 	List<Company> findByNameContaining(String name);
 	
 	// busqueda nombre en orden ascendente
